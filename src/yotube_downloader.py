81b50455cb8d98cfb25a53e171422bb7c5d9d155
Routybor <routybor@gmail.com>
2024-03-15 21:56:59 +0700

    venv

--- src/yotube_downloader.py
+++ src/yotube_downloader.py
@@ -1,9 +1,10 @@
 from glob import glob
 from os import path, remove, listdir
 from shutil import copy
-import re
+from re import sub
 from yt_dlp import YoutubeDL
 from mutagen.id3 import ID3, APIC
+
 
 def download_audio(url):
     ydl_opts = {
@@ -17,6 +18,7 @@ def download_audio(url):
     with YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
+
 def download_video(url):
     ydl_opts = {
         'format': 'bestvideo+bestaudio/best',
@@ -25,20 +27,22 @@ def download_video(url):
     with YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
+
 def audio_process(url):
     directory = './'
     pattern = r'\[.*?\]'
     mp3_files = glob(path.join(directory, "*.mp3"))
     for file in mp3_files:
-        cleaned_name = "Music/" + re.sub(pattern, '', file)
+        cleaned_name = "Music/" + sub(pattern, '', file)
         copy(file, cleaned_name)
-        # try:
-        preview_download(url)
-        preview_set(file)
-        preview_cleaner()
-        # except:
-        #     pass
+        try:
+            preview_download(url)
+            preview_set(file)
+            preview_cleaner()
+        except:
+            pass
         remove(file)
+
 
 def preview_set(file):
     another = "./maxresdefault*"
@@ -51,6 +55,7 @@ def preview_set(file):
         audio.add(apic)
         audio.save()
 
+
 def preview_cleaner():
     files = listdir("./")
     for file in files:
@@ -58,11 +63,13 @@ def preview_cleaner():
             file_path = path.join("./", file)
             remove(file_path)
 
+
 def preview_download(url):
     ydl = YoutubeDL()
     info_dict = ydl.extract_info(url, download=False)
     thumbnail_url = info_dict['thumbnail']
     ydl.download([thumbnail_url])
+
 
 yt_url = str(input("Video link = "))
 request = int(input("Audio/Video => 1/2 = "))
